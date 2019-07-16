7c94ea6fafbea8779349b6245104455fe2a72c6b
Valerie Manuel <valerie.manuel11@gmail.com>
2019-07-16 20:58:38 +0000

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,13 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+class Dancer
+  attr_accessor :name  
+  include Dance 
+  extend MetaDancing
+  # extend FancyDance::ClassMethods 
+  # include FancyDance::InstanceMethods
+  
+  def initialize (name)
+    @name = name
+  end
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,13 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+class Kid 
+  attr_accessor :name 
+  include Dance
+  extend MetaDancing
+  # extend FancyDance::ClassMethods 
+  # include FancyDance::InstanceMethods
+  
+  def initialize (name)
+    @name = name
+  end
+end
